ec1ac5d979dbf4c6cb7de8510d3747cda1943cce
chenyitao <285345240@qq.com>
2019-04-27 08:18:43 +0800

    修改项目名

---
+++ src/App/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="statusBar">
+        <span class="signal">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-xinhao"></use>
+          </svg>
+        </span>
+        <span class="brand">中国移动</span>
+        <span class="wifi">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-wifi"></use>
+          </svg>
+        </span>
+        <span class="blank"></span>
+        <span class="power">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-power-electricity"></use>
+          </svg>
+        </span>
+      </div>
+    </header>
+  </div>
+</template>
+
+
+
+<script src='./App.js'></script>
+<style lang="less" src='./App.less'></style>
+<style lang="less" src='../assets/reset.less'></style>
