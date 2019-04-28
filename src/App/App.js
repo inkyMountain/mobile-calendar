ceaf8ca92d213d9fa00c6ca365ecea07ac9b1718
chenyitao <285345240@qq.com>
2019-04-28 16:01:26 +0800

    repo更改为private

--- src/App/App.js
+++ src/App/App.js
@@ -2,5 +2,14 @@ export default {
     name: 'app',
     components: {
       
+    },
+    methods: {
+      test(event){
+        console.log(event);
+      }
+    },
+    mounted(){
+      let children = document.querySelector('#app').children
+      console.log(children);
     }
   }
--- src/App/App.vue
+++ src/App/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!------------------------Header------------------------->
     <header>
       <div class="statusBar">
         <span class="signal">
@@ -20,7 +21,129 @@
           </svg>
         </span>
       </div>
+      <div class="subStatus">
+        <div class="today">2017-9-14</div>
+        <div class="returnToToday" @click="test($event)">今天</div>
+      </div>
     </header>
+
+    <!-----------------------Main------------------------>
+    <main>
+        <div class="row weekdays">
+          <span>日</span>
+          <span>一</span>
+          <span>二</span>
+          <span>三</span>
+          <span>四</span>
+          <span>五</span>
+          <span>六</span>
+        </div>
+        <div class="row day">
+          <span>1</span>
+          <span>1</span>
+          <span>1</span>
+          <span>1</span>
+          <span>1</span>
+          <span>1</span>
+          <span>1</span>
+        </div>
+        <div class="row day">
+          <span>2</span>
+          <span>2</span>
+          <span>2</span>
+          <span>2</span>
+          <span>2</span>
+          <span>2</span>
+          <span>2</span>
+        </div>
+        <div class="row day">
+          <span>3</span>
+          <span>3</span>
+          <span>3</span>
+          <span>3</span>
+          <span>3</span>
+          <span>3</span>
+          <span>3</span>
+        </div>
+        <div class="row day">
+          <span>4</span>
+          <span>4</span>
+          <span>4</span>
+          <span>4</span>
+          <span>4</span>
+          <span>4</span>
+          <span>4</span>
+        </div>
+        <div class="row day">
+          <span>5</span>
+          <span>5</span>
+          <span>5</span>
+          <span>5</span>
+          <span>5</span>
+          <span>5</span>
+          <span>5</span>
+        </div>
+        <div class="foldMarker">
+          <div>
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-shang"></use>
+            </svg>
+          </div>
+        </div>
+    </main>
+    <section class="eventArea">
+      <div class="tabs">
+        <span class="selected">已设置提醒</span>
+        <span>创建时间</span>
+        <span>最后发言时间</span>
+      </div>
+      <div class="events-wrapper">
+        <div class="events">
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+        <div class="event"></div>
+      </div>
+      </div>
+    </section>
+
+    <!-----------------------Footer------------------------>
+    <footer class="menu">
+      <span class="power">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-lightning"></use>
+        </svg>
+        <div class="text">工作</div>
+      </span>
+      <span class="power">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-jiaose"></use>
+        </svg>
+        <div class="text">角色</div>
+      </span>
+      <span class="power">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-yuanquan"></use>
+        </svg>
+        <div class="text">事项圈</div>
+      </span>
+      <span class="power">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-tongshi"></use>
+        </svg>
+        <div class="text">同事</div>
+      </span>
+      <span class="power">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-rili"></use>
+        </svg>
+        <div class="text selected">日历</div>
+      </span>
+    </footer>
   </div>
 </template>
 
